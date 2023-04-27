0f10a0ed3767f0cffe98cd9e6b174c166af7876a
sam pandey <shamikantpandey559@gmail.com>
2023-04-27 22:19:57 +0530

    fetching data and displaying on conditions completed and front_end Script change

--- back_end/server.js
+++ back_end/server.js
@@ -20,22 +20,6 @@ app.use("/api", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-
-// Static files
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/front_end/build")));
-
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "front_end", "build", "index.html"))
-    );
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running..");
-    });
-}
-
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server Started on PORT ${PORT}`.yellow.bold);
