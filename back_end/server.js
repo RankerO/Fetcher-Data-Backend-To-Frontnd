339995aa112bc933a97f571806ef48fc6e89b8bf
Ankit pandey <91595780+RankerO@users.noreply.github.com>
2023-04-28 03:36:50 +0530

    Update server.js
    fetching data and displaying on conditions completed and front_end Script change

--- back_end/server.js
+++ back_end/server.js
@@ -20,7 +20,7 @@ app.use("/api", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT =5000
 app.listen(PORT, () => {
     console.log(`Server Started on PORT ${PORT}`.yellow.bold);
 });
