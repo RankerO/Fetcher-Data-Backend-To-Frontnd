be7ae343d75e2beec712f26f02eaf804960537b8
Ankit pandey <91595780+RankerO@users.noreply.github.com>
2023-04-29 06:47:42 +0530

    Update Three.js

--- front_end/src/pages/Three.js
+++ front_end/src/pages/Three.js
@@ -11,7 +11,7 @@ function Three() {
   const [userData, setUserData] = useState([]);
   const callAccount = async () => {
     try {
-      const res = await fetch('http://localhost:5000/api/StartswithM', {
+      const res = await fetch('https://fetcher-data.onrender.com/api/StartswithM', {
         method: 'GET',
         headers: {
 
