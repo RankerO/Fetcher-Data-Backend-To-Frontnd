7280625f48fb7b7bdfca84b31fda678da441e533
sam pandey <shamikantpandey559@gmail.com>
2023-04-29 06:23:54 +0530

    change packge.json

--- back_end/server.js
+++ back_end/server.js
@@ -8,6 +8,7 @@ const app = express();
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const userRoutes  = require("./routes/userRoutes");
 const connectDB = require("./db/connect");
+// const logger = require("./middleware/logger");
 app.use(cors());
 dotenv.config();
 connectDB();
@@ -19,6 +20,20 @@ app.use("/api", userRoutes);
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
+
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+//     app.get("*", (req, res) =>
+//         res.sendFile(
+//             path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//         )
+//     );
+// } else {
+//     app.get("/", logger, (req, res) => {
+//         res.send("Welcome to the home page");
+//     });
+// }
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
