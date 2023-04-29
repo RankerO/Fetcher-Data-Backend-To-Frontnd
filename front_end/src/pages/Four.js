cb9873e2f47bd72d025572dd7af1c24f7fc67443
Ankit pandey <91595780+RankerO@users.noreply.github.com>
2023-04-29 06:46:54 +0530

    Update Four.js

--- front_end/src/pages/Four.js
+++ front_end/src/pages/Four.js
@@ -11,7 +11,7 @@ function Four() {
   const [userData, setUserData] = useState([]);
   const callAccount = async () => {
     try {
-      const res = await fetch('http://localhost:5000/api/emailDoesNotIncludeAnyDigit', {
+      const res = await fetch('https://fetcher-data.onrender.com/api/emailDoesNotIncludeAnyDigit', {
         method: 'GET',
         headers: {
 
